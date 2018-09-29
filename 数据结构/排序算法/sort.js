3265108204c07ab9480c474261c9d0f7775bb2c3
CHENXPU <CHENXPU@foxmail.com>
2018-09-29 18:09:04 +0800

    feat(Sort): 添加冒泡排序和整理快速排序

---
+++ 数据结构/排序算法/sort.js
@@ -0,0 +1,12 @@
+module.exports = {
+  checkArray: array => {
+    if (!array || array.length <= 2) return;
+  },
+  swap: (array, left, right) => {
+    let rightValue = array[right];
+    array[right] = array[left];
+    array[left] = rightValue;
+    return;
+  },
+  Arr: [3, 4, 5, 9, 10, 2, 4, 6, 8, 11]
+};
---
+++ 数据结构/排序算法/冒泡排序.js
@@ -0,0 +1,13 @@
+const { checkArray, swap, Arr } = require('./sort');
+
+function bubble(array) {
+  checkArray(array);
+  for (let i = array.length - 1; i > 0; i--) {
+    // 从 0 到 `length - 1` 遍历
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) swap(array, j, j + 1);
+    }
+  }
+  return array;
+}
+console.log(bubble(Arr));
