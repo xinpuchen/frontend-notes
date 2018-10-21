2627f9f49cf5d703835063189ad064ae39ba0230
Zakary <zakarycode@gmail.com>
2018-10-21 12:23:07 +0800

    1 4 5

---
+++ 面试经历/面试题/字节跳动/4.js
@@ -0,0 +1,71 @@
+/**
+ * 给定一个一维的正整数数组，逐次选择其中一个数做消除，
+ * 消除所获得的分数为当前数字和左右相邻数字的乘积（当左边或者右边没有数字可以认为是1）。
+ * e.g.输入数组：[3, 1, 5, 8]
+ * ·step1：消除1，获得分数15=3*1*5，数组变为[3, 5, 8]
+ * ·step2：消除5，获得分数120=3*5*8，数组变为[3, 8]
+ * ·step3：消除3，获得分数24=3*8，数组变为[8]
+ * ·step4：消除8，获得分数8=8，数组变为[]
+ * 最终获得分数：15+120+24+8=167
+ * 求消除能够获取的最大分数w
+ * 输入描述：
+ * 第一行为数组的长度，后面依次为数组内的数字：
+ * 4
+ * 3
+ * 1
+ * 5
+ * 8
+ * 输出描述：
+ * 输出数字，对应分数，如167
+ *
+ * 示例1 输入输出示例仪供调试，后台判题效据一般不包含示例
+ * 输入
+ * 4
+ * 3
+ * 1
+ * 5
+ * 8
+ * 输出
+ * 167
+ * 备注：
+ * 数据范围
+ * 前30%的数据，小数据集（n<10）
+ * 中间30%的数据，中等数据集（n<30）
+ * 后40%的数据，大数据集（50<=n<=200）
+ */
+const N = 4,
+  ARRAY = [
+    3, 1, 5, 8
+  ],
+  filterMin = (arr = ARRAY) => {
+    let min = null;
+    if (arr.length >= 3) {
+      min = Math.min(...arr.slice(1, -1));
+    } else {
+      min = Math.min(...arr);
+    }
+    let score = 0;
+    arr = arr.filter((e, i, a) => {
+      if (e === min) {
+        score = e;
+        if (i > 0)
+          score *= a[i - 1];
+        if (i < a.length - 1)
+          score *= a[i + 1];
+        // console.log(e, score);
+      }
+      return e !== min;
+    });
+    return {score: score, arr: arr};
+  },
+  main = (n = N, arr = ARRAY) => {
+    let score = 0;
+    for (let index = 0; index < 4; index++) {
+      const e = filterMin(arr);
+      score += e.score;
+      arr = e.arr;
+    }
+    return score;
+  };
+
+console.log(`消除能够获取的最大分数为${main()}`)
---
+++ 面试经历/面试题/字节跳动/5.js
@@ -0,0 +1,63 @@
+/**
+ * 给定序列a：a1、a2、……、an 从左到右依次操作a[i]，生成新序列：
+ * a[1]直接放新序列，从第二个数开始若a[i]>a[i-1]，放入当前新序列最右边，否则放入序列最左边，
+ * 得到新序列b：b1、b2、……、bn
+ * 现给定最终新序列，求原始序列有多少种可能?
+ * 输入描述：
+ * 第一行一个数n
+ * 第二行n个数表示bi，且各不相同
+ * 输出描述：
+ * 一行一个数m，满足情况的原始序列数量mod 12345678
+ *
+ * 示例1 输入输出示例仅供调试，后台判题数据一般不包含示例
+ * 4
+ * 1 2 3 4
+ * 输出
+ * 8
+ * 备注：
+ * 数据约定：
+ * n <= 1000
+ * bi <= 5000
+ */
+const N = 4,
+  ARRAY = [
+    1, 2, 3, 4
+  ],
+  pushCheck = (n = 0, arr = [], isR) => {
+    arr = arr.map((e) => {
+      if (isR && e[e.length - 1] > n || !isR && e[e.length - 1] < n) {
+        e.push(n);
+        return e;
+      } else {
+        return false;
+      }
+    }).filter(e => !!e)
+    return arr;
+  },
+  getWay = (arr = ARRAY) => {
+    if (arr.length >= 2) {
+      // const f=0,fArr = [arr[f]], e=arr.length-1,eArr = [arr[e]];
+      const arr1 = arr.map(e => e),
+        arr2 = arr.map(e => e),
+        s = arr1.shift(),
+        e = arr2.pop(),
+        sArr = pushCheck(s, getWay(arr1), true),
+        eArr = pushCheck(e, getWay(arr2), false);
+      // debugger
+      const res = [
+        ...sArr,
+        ...eArr
+      ];
+      return res;
+    } else {
+      // console.log(arr); debugger
+      return [arr];
+    }
+  },
+  mod = (n = N, arr = ARRAY) => {
+    const array = getWay(arr);
+    // debugger console.log(...array);
+    return array.length;
+  };
+const M = mod();
+console.log(`满足情况的原始序列数量mod为${M}`);
