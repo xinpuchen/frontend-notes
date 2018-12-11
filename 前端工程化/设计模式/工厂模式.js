761faa54fc014be3cd5995f0ccebf4b0c9c2d401
xinpu <xinpuchen@126.com>
2018-12-11 11:34:31 +0800

    fix(Model): 修改工厂模式

--- 前端工程化/设计模式/工厂模式.js
+++ 前端工程化/设计模式/工厂模式.js
@@ -1,5 +1,6 @@
 /**
  * 工厂起到的作用就是隐藏了创建实例的复杂度，只需要提供一个接口，简单清晰。
+ * 缺点：对象无法识别，因为所有的实例都指向一个原型
  */
 
 class Man {
@@ -18,3 +19,17 @@ class Factory {
 }
 
 Factory.create("cxp").getName();
+
+function createPerson(name) {
+  var p = new Object();
+  p.name = name;
+  p.getName = function() {
+    console.log(this.name);
+  };
+  return p;
+}
+
+var person = createPerson("kevin");
+person.getName();
+console.log(person instanceof createPerson);
+console.log(person instanceof Object);
