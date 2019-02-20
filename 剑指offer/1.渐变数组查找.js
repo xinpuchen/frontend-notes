b0f136a3474580a6effa296b42aee20d06008732
xinpu <xinpuchen@126.com>
2019-02-21 00:15:13 +0800

    chore: 剑指 offer

---
+++ 剑指offer/1.渐变数组查找.js
@@ -0,0 +1,28 @@
+/**
+ * 题目描述
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+
+const find = (
+  target = 9,
+  array = [[1, 2, 3, 4, 5, 6, 7], [2, 3, 4, 5, 6, 7, 8], [3, 4, 5, 6, 7, 8, 9]]
+) => {
+  let colLength = array.length;
+  let rowLength = array[0].length;
+  let col = colLength - 1;
+  let row = 0;
+  while (row < rowLength && col >= 0) {
+    if (array[col][row] < target) {
+      row++;
+      continue;
+    } else if (array[col][row] > target) {
+      col--;
+      continue;
+    } else {
+      return console.log("找到啦！");
+    }
+  }
+  return false;
+};
+find();
