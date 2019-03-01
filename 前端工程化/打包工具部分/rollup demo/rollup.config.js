83491f32980849f4e144932f8f3b6d851ec47f52
ZakaryCode <zakarycode@gmail.com>
2019-03-01 12:45:44 +0800

    feat: add html for web socket and a rollop-easy-demo

---
+++ 前端工程化/打包工具部分/rollup demo/rollup.config.js
@@ -0,0 +1,23 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  input: "app.js",
+  output: {
+    file: "public/main.js",
+    format: "umd",
+    name: 'bundle-name'
+  },
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
--- 浏览器/web-socket/app.js
+++ 浏览器/web-socket/app.js
@@ -8,40 +8,45 @@ const wss = new WebSocketServer({
 
 wss.on('connection', function (ws) {
     console.log(`[SERVER] connection()`);
+    let mesTimer;
     ws.on('message', function (message) {
         console.log(`[SERVER] Received: ${message}`);
-        setTimeout(() => {
+        mesTimer = setTimeout(() => {
             ws.send(`What's your name?`, (err) => {
                 if (err) {
                     console.log(`[SERVER] Received: ${err}`);
                 }
             });
         }, 1000);
-    })
+    });
+    ws.on('close', function (message) {
+        console.log(`[SERVER] close()`);
+        mesTimer && clearTimeout(mesTimer);
+    });
 });
 
 console.log('ws server started at port 3000...');
 
-// client test:
+// // client test:
 
-let count = 0;
+// let count = 0;
 
-let ws = new WebSocket('ws://localhost:3000/ws/chat');
+// let ws = new WebSocket('ws://localhost:3000/ws/chat');
 
-ws.on('open', function () {
-    console.log(`[CLIENT] open()`);
-    ws.send('Hello!');
-});
+// ws.on('open', function () {
+//     console.log(`[CLIENT] open()`);
+//     ws.send('Hello!');
+// });
 
-ws.on('message', function (message) {
-    console.log(`[CLIENT] Received: ${message}`);
-    count++;
-    if (count > 3) {
-        ws.send('Goodbye!');
-        ws.close();
-    } else {
-        setTimeout(() => {
-            ws.send(`Hello, I'm Mr No.${count}!`);
-        }, 1000);
-    }
-});
+// ws.on('message', function (message) {
+//     console.log(`[CLIENT] Received: ${message}`);
+//     count++;
+//     if (count > 3) {
+//         ws.send('Goodbye!');
+//         ws.close();
+//     } else {
+//         setTimeout(() => {
+//             ws.send(`Hello, I'm Mr No.${count}!`);
+//         }, 1000);
+//     }
+// });
