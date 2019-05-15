b146f1974316e658dd7fac9f498465e25f964ed6
ZakaryCode <zakarycode@gmail.com>
2019-05-15 16:43:27 +0800

    feat: leetcode

---
+++ leetcode/easy/35.搜索插入位置.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (42.86%)
+ * Likes:    247
+ * Dislikes: 0
+ * Total Accepted:    44.3K
+ * Total Submissions: 102.2K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  const n = nums.findIndex(e => e >= target);
+  return n === -1 ? nums.length : n;
+};
+
---
+++ leetcode/medium/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (35.28%)
+ * Likes:    121
+ * Dislikes: 0
+ * Total Accepted:    16.3K
+ * Total Submissions: 45.4K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ *
+ * 如果数组中不存在目标值，返回 [-1, -1]。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: [3,4]
+ *
+ * 示例 2:
+ *
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: [-1,-1]
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  let l = 0, r = nums.length -1;
+  while (l < r && (nums[l] !== target || nums[r] !== target))
+    if (nums[l] !== target) l++; else r--;
+  return nums[l] === target && nums[r] === target ? [l, r] : [-1, -1];
+};
+
