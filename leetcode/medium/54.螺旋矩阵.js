19250f2618911685dcdcd1bb9b4b772fe6f607b8
ZakaryCode <zakarycode@gmail.com>
2019-06-05 11:59:32 +0800

    feat(leetcode): weekly

---
+++ leetcode/medium/54.螺旋矩阵.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=54 lang=javascript
+ *
+ * [54] 螺旋矩阵
+ *
+ * https://leetcode-cn.com/problems/spiral-matrix/description/
+ *
+ * algorithms
+ * Medium (34.10%)
+ * Likes:    160
+ * Dislikes: 0
+ * Total Accepted:    14.9K
+ * Total Submissions: 41.8K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * [
+ * ⁠[ 1, 2, 3 ],
+ * ⁠[ 4, 5, 6 ],
+ * ⁠[ 7, 8, 9 ]
+ * ]
+ * 输出: [1,2,3,6,9,8,7,4,5]
+ *
+ *
+ * 示例 2:
+ *
+ * 输入:
+ * [
+ * ⁠ [1, 2, 3, 4],
+ * ⁠ [5, 6, 7, 8],
+ * ⁠ [9,10,11,12]
+ * ]
+ * 输出: [1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ *
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+  const array = [];
+  let i = j = f = 0;
+  while (matrix.length > 0 && matrix[0].length > 0 && array.length < matrix.length * matrix[0].length) {
+    array.push(matrix[i][j]);
+    if (i === f && j < matrix[i].length - f - 1) {
+      j++;
+    } else if (j === matrix[i].length - f - 1 && i < matrix.length - f - 1) {
+      i++;
+    } else if (i === matrix.length - f - 1 && j > f) {
+      j--;
+    } else if (j === f && i > f + 1) {
+      i--;
+    } else if (j === f && i === f + 1) {
+      f++;
+      j++;
+    }
+  }
+  return array;
+};
+
+// spiralOrder([]);
+// spiralOrder([[1,2],[3,4]]);
+// spiralOrder([[1,2,3],[4,5,6],[7,8,9]]);
+// spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]);
---
+++ leetcode/medium/55.跳跃游戏.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=55 lang=javascript
+ *
+ * [55] 跳跃游戏
+ *
+ * https://leetcode-cn.com/problems/jump-game/description/
+ *
+ * algorithms
+ * Medium (33.83%)
+ * Likes:    193
+ * Dislikes: 0
+ * Total Accepted:    18.6K
+ * Total Submissions: 53K
+ * Testcase Example:  '[2,3,1,1,4]'
+ *
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个位置。
+ *
+ * 示例 1:
+ *
+ * 输入: [2,3,1,1,4]
+ * 输出: true
+ * 解释: 从位置 0 到 1 跳 1 步, 然后跳 3 步到达最后一个位置。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: [3,2,1,0,4]
+ * 输出: false
+ * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+ *
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let index = 0;
+  let index0 = -1;
+  let index1 = 0;
+  while ((index0 = nums.indexOf(0, index0 + 1)) !== -1) {
+    while (index1 <= index0) {
+      index1 = Math.max(index1, index + nums[index]);
+      if (index1 >= nums.length - 1) {
+        return true;
+      }
+      if (index < index0) {
+        index++;
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+};
